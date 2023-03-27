60335180222308796cd55337936af971f7032961
박선우 <o_o_ost@naver.com>
2023-03-27 23:44:50 +0900

    게시판 UI 구현
    게시판 UI 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BoardList from './Component/BoardList';
+import Write from './Component/Write';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <BoardList />
+      <Write />
     </div>
   );
 }
